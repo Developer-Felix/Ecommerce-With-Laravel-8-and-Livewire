ccda268755c29a01e8fdf090be87731bf30aed46
Developer-felix <onjombafelix1@gmail.com>
2022-03-24 02:50:26 -0700

    Admin and user authentication

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\AboutComponent;
 use App\Http\Livewire\ContactComponent;
-
+use App\Http\Livewire\ThankYouComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -24,3 +26,19 @@ Route::get('/checkout',CheckoutComponent::class);
 Route::get('/about',AboutComponent::class);
 
 Route::get('/contact',ContactComponent::class);
+
+Route::get('/thankyou',ThankYouComponent::class);
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+//For User or Customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+  Route::get('/users/dashboard',UserDashboardComponent::class)->name('user.dashboard');
+});
+
+//For ADM
+Route::middleware(['auth:sanctum', 'verified','authadmin'])->group(function(){
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+});
